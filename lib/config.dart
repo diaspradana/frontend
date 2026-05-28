198193981f41a41133155fa1c8f107a6fd444a5f
diaspradana <daniswaradias87@gmail.com>
2026-05-29 00:46:02 +0700

    update config ke railway

--- lib/config.dart
+++ lib/config.dart
@@ -2,6 +2,12 @@ import 'package:flutter/foundation.dart';
 
 class AppConfig {
   static String get baseUrl {
+    // Mode Produksi (Release APK): Mengarah ke server Railway
+    if (kReleaseMode) {
+      return 'https://keuanganrtrw.up.railway.app';
+    }
+
+    // Mode Pengembangan (Debug/Local)
     if (kIsWeb) {
       return 'http://localhost:5000';
     }
